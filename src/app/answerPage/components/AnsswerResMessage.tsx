36e578cca05afca3b6c2c9e6aec0a7f55f9bb374
kuwa124 <c0mwgave1407@gmail.com>
2024-08-22 15:35:01 +0900

    🐛 回答受付コンポーネントの修正

--- src/app/answerPage/components/AnsswerResMessage.tsx
+++ src/app/answerPage/components/AnsswerResMessage.tsx
@@ -4,14 +4,12 @@ import { useRecoilState } from 'recoil';
 
 export const AnsswerMessage: React.FC = () => {
   const [isAccepting] = useRecoilState(respAcceptToggleAtom);
-
+  if (!isAccepting) {
+    return null;
+  }
   return (
-    <div
-      className={`flex justify-center items-center container mx-auto p-4 max-w-3xl text-gray-600  rounded-lg   mb-4 ${
-        isAccepting ? 'bg-white shadow-md' : ''
-      }`}
-    >
-      {isAccepting ? '回答を受け付けています' : ''}
+    <div className='flex justify-center items-center container mx-auto p-4 max-w-3xl text-gray-600  rounded-lg mb-4 bg-white shadow-md'>
+      回答を受け付けています
     </div>
   );
 };
