2ab443960981d497188683cf476fff481fe1eb38
kuwa124 <c0mwgave1407@gmail.com>
2024-08-27 16:10:33 +0900

    🚧 設定ページを作成中

--- src/app/SettingPage/Setting.tsx
+++ src/app/SettingPage/Setting.tsx
@@ -1,6 +1,7 @@
 'use client'; // クライアントサイドでの実行を明示
 
-import { ToggleButton } from '@/app/components/ToggleButton';
+import { AccordionItem } from '@/app/components/tool/AccordionItem';
+import { ToggleButton } from '@/app/components/tool/ToggleButton';
 import { useCallback, useState } from 'react';
 
 export const Setting = () => {
@@ -15,14 +16,21 @@ export const Setting = () => {
     <div className='max-w-4xl mx-auto w-full text-gray-600'>
       <div className='bg-white rounded-lg shadow-md p-6 mb-4'>
         <h1 className='text-xl pb-4 border-b border-b-slate-300'>設定</h1>
-        <div className='flex justify-between items-center'>
-          <div className='py-8 border-b border-b-slate-300    '>
+        <div className='flex justify-between items-center py-8 border-b border-b-slate-300'>
+          <div className=''>
             <h2 className='text-lg'>テストにする</h2>
             <p className=''>
               点数の割り当て、解答の設定、フィードバックの自動提供が可能になります
             </p>
           </div>
           <ToggleButton isChecked={isTest} onChange={toggleTest} />
+        </div>
+        <div className='flex justify-between items-center py-8 border-b border-b-slate-300'>
+          <div className=''>
+            <h2 className='text-lg'>回答</h2>
+            <p className=''>回答を収集、保護する方法を管理できます </p>
+          </div>
+          <AccordionItem />
         </div>
       </div>
     </div>
