1749be3bbb550332b1d3b5e97a6b9020bbbd38dc
kuwa124 <c0mwgave1407@gmail.com>
2024-10-09 14:27:44 +0900

    ♻️ メタデータの修正と細かなリファクタリング

--- src/app/components/GoogleFormClone.tsx
+++ src/app/components/GoogleFormClone.tsx
@@ -15,7 +15,7 @@ const GoogleFormClone: React.FC = () => {
   // 質問を削除する関数
   const removeQuestion = useCallback(
     (id: string) => {
-      setQuestions(questions.filter((q) => q.id !== id));
+      setQuestions(questions.filter((question) => question.id !== id));
     },
     [questions, setQuestions]
   );
@@ -24,7 +24,7 @@ const GoogleFormClone: React.FC = () => {
   const updateQuestion = useCallback(
     (id: string, updates: Partial<Question>) => {
       setQuestions(
-        questions.map((q) => (q.id === id ? { ...q, ...updates } : q))
+        questions.map((question) => (question.id === id ? { ...question, ...updates } : question))
       );
     },
     [questions, setQuestions]
