6fa4962bf178ccf01cb01d0cda67d308b632aea5
kuwa124 <c0mwgave1407@gmail.com>
2024-10-10 10:01:48 +0900

    📱 フォームをレスポンシブ対応

--- src/app/components/FormHeader.tsx
+++ src/app/components/FormHeader.tsx
@@ -18,7 +18,7 @@ const FormHeader: React.FC = () => {
 
   return (
     // フォームのヘッダー部分 - 背景色、角丸、影、左と上のボーダーを設定
-    <div className='bg-white rounded-lg shadow-md p-6 mb-4 border-t-8 border-l-8 border-t-purple-600 border-l-blue-600 w-[900px]'>
+    <div className='bg-white rounded-lg shadow-md p-6 mb-4 border-t-8 border-l-8 border-t-purple-600 border-l-blue-600 sm:w-[500px] md:w-[600px] lg:w-[800px]'>
       {/* フォームのタイトル入力欄 */}
       <div className='relative'>
         <input
