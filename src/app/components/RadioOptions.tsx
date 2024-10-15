2407840f5f40e88f2f99f371dbd2e28ef22fa6d8
kuwa124 <c0mwgave1407@gmail.com>
2024-10-15 15:45:52 +0900

    🚧 ラジオボタン作業中

--- src/app/components/RadioOptions.tsx
+++ src/app/components/RadioOptions.tsx
@@ -15,10 +15,14 @@ export const RadioOptions = ({ type }: RadioOptionProps) => {
     newOptions[index] = value;
     setOptions(newOptions);
   };
+  const removeOption = (index: number) => {
+    setOptions(options.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
-      {(options || []).map((option, index) => (
-        <div key={index} className='flex items-center mb-2'>
+      {options.map((option, index) => (
+        <div key={index} className='flex items-center mb-2 group'>
           <input
             type={type === 'multipleChoice' ? 'radio' : 'checkbox'}
             id={`option-${index}`}
@@ -33,6 +37,13 @@ export const RadioOptions = ({ type }: RadioOptionProps) => {
             className='flex-grow w-full border-b border-b-white hover:border-b-gray-200 p-2 bg-white  focus:border-b-purple-500'
             placeholder={`選択肢 ${index + 1}`}
           />
+          {options.length > 1 && (
+            <button
+              className='opacity-0 group-hover:opacity-100 ml-2 text-gray-400 '
+              title='削除'
+              onClick={() => removeOption(index)}
+            ></button>
+          )}
         </div>
       ))}
       <div className='flex items-center mb-2'>
@@ -45,9 +56,13 @@ export const RadioOptions = ({ type }: RadioOptionProps) => {
         />
         <button
           onClick={addOption}
-          className='text-gray-400 hover:underline mr-2'
+          className='text-gray-400 border-b-2 border-b-white hover:border-b-gray-200 mr-2'
         >
           選択肢を追加
+        </button>
+        <p>または</p>
+        <button onClick={addOption} className='text-blue-400 mr-2'>
+          「その他」を追加
         </button>
       </div>
     </div>
