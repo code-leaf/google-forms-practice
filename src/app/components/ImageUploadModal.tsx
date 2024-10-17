7eeebd865c8c599104c02d1acea592f19e5c7037
kuwa124 <c0mwgave1407@gmail.com>
2024-10-17 12:56:51 +0900

    🚧 クラウドの画像を挿入、作業中

--- src/app/components/ImageUploadModal.tsx
+++ src/app/components/ImageUploadModal.tsx
@@ -1,4 +1,4 @@
-import { faX } from '@fortawesome/free-solid-svg-icons';
+import { faCloud, faX } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type ImageUploadModalProps = {
@@ -15,7 +15,7 @@ export const ImageUploadModal = ({
   return (
     <div className='fixed inset-0 flex items-center justify-center z-50'>
       {/* 実際の操作画面 */}
-      <div className='bg-white w-96 border border-gray-400 shadow-lg pb-10'>
+      <div className='bg-white w-1/2 border border-gray-400 shadow-lg pb-10'>
         <div className='p-6'>
           {/* ヘッダー部分 */}
           <div className='flex justify-between items-center'>
@@ -31,12 +31,32 @@ export const ImageUploadModal = ({
         </div>
 
         {/* ツールバー */}
-        <div className='space-x-2 px-6'>
+        <div className='space-x-4 px-6 flex'>
           <p className='inline-block text-blue-500 py-3 border-b-2 border-b-blue-500 '>
             アップロード
           </p>
+          <p className='text-gray-500 py-3 border-b-2 border-white'>
+            ウェブカメラ
+          </p>
+          <p className='text-gray-500 py-3 border-b-2 border-white'>URL</p>
+          <p className='text-gray-500 py-3 border-b-2 border-white'>写真</p>
+          <p className='text-gray-500 py-3 border-b-2 border-white'>
+            Googleドライブ
+          </p>
+          <p className='text-gray-500 py-3 border-b-2 border-white'>
+            Google画像検索
+          </p>
         </div>
         <hr />
+
+        <div className='flex justify-center items-center p-8 relative'>
+          <FontAwesomeIcon icon={faCloud} className='text-9xl text-gray-400' />
+
+          <FontAwesomeIcon
+            icon={faCloud}
+            className='text-8xl text-gray-300 absolute top-20 left-96'
+          />
+        </div>
       </div>
     </div>
   );
