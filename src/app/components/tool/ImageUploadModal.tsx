9ea02a8d3a69777e4c1c6e8cb55f42301be85e05
kuwa124 <c0mwgave1407@gmail.com>
2024-10-25 13:45:09 +0900

    ♻️ 画像添付モーダルのツールバー、テキストサイズ変更

--- src/app/components/tool/ImageUploadModal.tsx
+++ src/app/components/tool/ImageUploadModal.tsx
@@ -15,7 +15,7 @@ export const ImageUploadModal = ({
   return (
     <div className='fixed inset-0 flex items-center justify-center z-50'>
       {/* 実際の操作画面 */}
-      <div className='bg-white w-1/2 border border-gray-400 shadow-lg pb-10'>
+      <div className='bg-white border border-gray-400 shadow-lg pb-10'>
         <div className='p-6'>
           {/* ヘッダー部分 */}
           <div className='flex justify-between items-center'>
@@ -31,7 +31,7 @@ export const ImageUploadModal = ({
         </div>
 
         {/* ツールバー */}
-        <div className='space-x-4 px-6 flex'>
+        <div className='space-x-4 px-6 flex text-xs'>
           <p className='inline-block text-blue-500 py-3 border-b-2 border-b-blue-500 '>
             アップロード
           </p>
@@ -46,7 +46,7 @@ export const ImageUploadModal = ({
           <p className='text-gray-500 py-3 border-b-2 border-white'>
             Google画像検索
           </p>
-          <p className='text-gray-500 py-3 border-b-2 border-white'>
+          <p className='text-gray-500 py-3 border-b-2 border-white text-end'>
             ※画面切り替えはできません。
           </p>
         </div>
