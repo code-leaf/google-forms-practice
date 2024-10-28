2d19cd178087063e453a847923f7a03d9b603b14
kuwa124 <c0mwgave1407@gmail.com>
2024-10-28 10:53:57 +0900

    💡 🔥 コメントの訂正と不要なエクスポートの削除

--- src/store/RespAcceptToggleAtom.ts
+++ src/store/RespAcceptToggleAtom.ts
@@ -1,9 +1,6 @@
 import { atom } from 'recoil';
 
-// 質問の型定義
-export type RespAcceptToggle = {};
-
-// 質問リストを管理するRecoil atom
+// 回答の受け付け状況を管理するRecoil atom
 export const respAcceptToggleAtom = atom<boolean>({
   key: 'respAcceptToggleAtom', // ユニークなキー
   default: true, // デフォルト値はtrue
