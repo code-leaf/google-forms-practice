ae64667a89ccce286a2cfb2599a4727e36bc33bb
kuwa124 <c0mwgave1407@gmail.com>
2024-11-12 12:48:17 +0900

    🎨 プロビューページに送信ボタンを等を追加（機能未実装）

--- src/app/formPreview/page.tsx
+++ src/app/formPreview/page.tsx
@@ -10,14 +10,25 @@ const FormPreview = () => {
 
   return (
     <main className='min-h-screen bg-purple-100'>
-      <div className='flex justify-center'>
-        <div className='flex flex-col'>
-          <PreviewHeader />
-          {/* 質問のリスト */}
-          {questions.map((question) => (
-            <PreviewAnswer key={question.id} questionId={question.id} />
-          ))}
+      <div className='flex flex-col  items-center'>
+        <div className='flex justify-center'>
+          <div className='flex-col'>
+            <PreviewHeader />
+            {/* 質問のリスト */}
+            {questions.map((question) => (
+              <PreviewAnswer key={question.id} questionId={question.id} />
+            ))}
+            <div className='flex justify-between items-center'>
+              <button className='px-4 py-2 w-20 bg-purple-800 text-white rounded hover:bg-purple-700 transition'>
+                送信
+              </button>
+              <button className='px-4 py-2 text-purple-800 hover:bg-opacity-20 hover:bg-purple-200'>
+                フォームをクリア
+              </button>
+            </div>
+          </div>
         </div>
+        <div className='text-gray-600 text-2xl mt-4'>Google Form練習</div>
       </div>
     </main>
   );
