11b70dc0dd2ba095e616f944157d7c5401837faa
kuwa124 <c0mwgave1407@gmail.com>
2024-11-13 15:38:29 +0900

    ✨ プレビューの均等目盛レイアウト作成
    🚧 クリック関数の実装が作業中

--- src/app/formPreview/components/PreviewQuestionType/PreviewLinearScale.tsx
+++ src/app/formPreview/components/PreviewQuestionType/PreviewLinearScale.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import { endLinearScaleAtom } from '@/store/EndLinearScaleAtom';
+import { startLinearScaleAtom } from '@/store/StartLinearScaleAtom';
+import React, { useState } from 'react';
+import { useRecoilValue } from 'recoil';
 
-export const PreviewLinearScale = () => {
+type PreviewLinearScaleProps = {
+  questionId: string;
+};
+
+export const PreviewLinearScale = ({ questionId }: PreviewLinearScaleProps) => {
+  // Recoilから開始値と終了値を取得
+  const start = useRecoilValue(startLinearScaleAtom);
+  const end = useRecoilValue(endLinearScaleAtom);
+
+  // 選択された値を管理するstate
+  const [selectdLineRadio, setSelectdLineRadio] = useState<string>('');
+
+  /** startからendまでの連続した数値を配列として作成
+   * @property - 第一引数: 配列の長さ
+   * @property - 第二引数: 各要素の値
+   */
+  const numbers: number[] = Array.from(
+    { length: end - start + 1 },
+    (_, i) => start + i
+  );
+
+  const handleLineRadioChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSelectdLineRadio(event.target.value);
+  };
+
   return (
-    <div>PreviewLinearScale</div>
-  )
-}
+    <div className='flex justify-between items-center'>
+      {numbers.map((number) => (
+        <div key={number} className='flex-col'>
+          <div className='text-sm mb-2'>{number}</div>
+          <input
+            type='radio'
+            name='LinearScale'
+            value={number.toString()}
+            id={`LinearScale-${number}`}
+            checked={selectdLineRadio === number.toString()}
+            className='h-4 w-4 cursor-pointer'
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
